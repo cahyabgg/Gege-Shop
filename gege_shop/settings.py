851ac63745effb5454a65370a595ad9e65636778
cahyacoding <cahya.bgg@gmail.com>
2024-09-18 12:57:55 +0700

    Menambah folder .github dan menambah trusted origin di setting

--- gege_shop/settings.py
+++ gege_shop/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","http://cahya-bagus-mentalhealthtracker.pbp.cs.ui.ac.id/",
+                        "https://cahya-bagus-mentalhealthtracker.pbp.cs.ui.ac.id/"]

b2d2ba0ab35bc30fc600632c7968696cd660a9bb
cahyacoding <cahya.bgg@gmail.com>
2024-09-19 16:05:56 +0700

    Menyelesaikan tugas 3

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from main.views import (
     show_main,create_product,show_json,
-    show_json_by_id,show_xml,show_xml_by_id
+    show_json_by_id,show_xml,show_xml_by_id,
+    register,user_login,logout_user,
+    edit_product,delete_product
     )
 
 app_name = 'main'
@@ -12,5 +14,10 @@ urlpatterns = [
     path("json/",show_json, name= "show_json"),
     path("xml/",show_xml, name= "show_xml"),
     path("json/<int:id>/",show_json_by_id,name="show_json_by_id"),
-    path("xml/<int:id>/",show_xml_by_id,name="show_xml_by_id")
+    path("xml/<int:id>/",show_xml_by_id,name="show_xml_by_id"),
+    path("login",user_login,name= "login"),
+    path("register/",register,name="register"),
+    path("logout/",logout_user,name="logout"),
+    path('edit-product/<int:id>', edit_product, name='edit_product'),
+    path('delete/<int:id>', delete_product, name='delete_product'), 
 ]
--- main/views.py
+++ main/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render,redirect
 from main.models import Product
 from main.forms import ProductForm
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
-
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.urls import reverse
 # Create your views here.
 
+@login_required(login_url='/login')
 def show_main(request):
     product = Product.objects.all()
 
@@ -13,7 +19,8 @@ def show_main(request):
         "name" : "Cahya Bagus Gautama Gozales",
         "npm" : "2306275380",
         "class" : "PBP C",
-        "products" : product 
+        "products" : product,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request,"main.html",context)
@@ -46,3 +53,62 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = Product.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+
+
+def user_login(request):
+    if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+def edit_product(request, id):
+    # Get book berdasarkan ID
+    product = Product.objects.get(pk = id)
+
+    # Set book sebagai instance dari form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Simpan form dan kembali ke halaman awal
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "edit_book.html", context)
+
+def delete_product(request, id):
+    # Get data berdasarkan ID
+    product = Product.objects.get(pk = id)
+    # Hapus data
+    product.delete()
+    # Kembali ke halaman awal
+    return HttpResponseRedirect(reverse('main:show_main'))
